28de877e87c1825c8b5e12814df1d02cf25350e7
Atul Pandey <77284214+shad0w0P@users.noreply.github.com>
2025-07-02 14:34:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import Main from './Components/Main/Main';
+import NavBar from './Components/NavBar/NavBar';
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <Home/>
+      <Main/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
